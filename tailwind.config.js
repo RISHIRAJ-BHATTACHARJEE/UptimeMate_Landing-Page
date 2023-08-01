c2a4c52fe48c69ed59b26fb06b5bc52d06bd0aac
Rishiraj Bhattacharjee <rishirajbhattacharjee974@gmail.com>
2023-08-01 21:15:03 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'btn-clr': '#4f46e5',
+        'inner-1': 'inset 0 3px 5px 0 rgba(0, 0, 0, 0.06)',
+      }
+    },
+  },
+  plugins: [],
+}
+
